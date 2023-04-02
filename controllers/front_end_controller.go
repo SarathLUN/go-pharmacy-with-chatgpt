d34d432319ed905dc32aaf969932c69bef0c77f2
SarathLUN <lun.sarath@gmail.com>
2023-04-02 17:08:55 +0700

    - set dynamic active menu

--- controllers/front_end_controller.go
+++ controllers/front_end_controller.go
@@ -6,21 +6,31 @@ import (
 )
 
 func LoadHomePage(c *gin.Context) {
-	c.HTML(http.StatusOK, "home.gohtml", gin.H{})
+	c.HTML(http.StatusOK, "home.gohtml", gin.H{
+		"Active": "home",
+	})
 }
 
 func LoadShopPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "shop.gohtml", gin.H{})
+	c.HTML(http.StatusOK, "shop.gohtml", gin.H{
+		"Active": "shop",
+	})
 }
 
 func LoadShopCartPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "shop-cart.gohtml", gin.H{})
+	c.HTML(http.StatusOK, "shop-cart.gohtml", gin.H{
+		"Active": "shop-cart",
+	})
 }
 
 func LoadAboutUsPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "about-us.gohtml", gin.H{})
+	c.HTML(http.StatusOK, "about-us.gohtml", gin.H{
+		"Active": "about-us",
+	})
 }
 
 func LoadContactUsPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "contact-us.gohtml", gin.H{})
+	c.HTML(http.StatusOK, "contact-us.gohtml", gin.H{
+		"Active": "contact-us",
+	})
 }
